3865e2a2c666806b90ffa8ae2824f855779489e8
hannah-developer <hannahchoi.developer@gmail.com>
2021-04-22 03:59:40 +0100

    ReactMapGL & added coord change on input

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import mapboxgl from "mapbox-gl";
+import ReactMapGL, { NavigationControl, FlyToInterpolator } from "react-map-gl";
 import Sidebar from "./Components/Sidebar";
 
 function App() {
-    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-    const mapRef = useRef(null);
+    const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-    const [lat, setLat] = useState(-0.14296254515647888);
-    const [lng, setLng] = useState(51.541072845458984);
-    const [zoom, setZoom] = useState(16);
+    const [viewport, setViewport] = useState({
+        latitude: 51.541,
+        longitude: -0.1429,
+        width: "100%",
+        height: "100%",
+        zoom: 16,
+    });
 
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapRef.current,
-            style: "mapbox://styles/hannah-developer/cknrt5a4011lh17mseb8f1kt4",
-            center: [lat, lng],
-            zoom: zoom,
-        });
-
-        map.on("move", () => {
-            setLng(map.getCenter().lng.toFixed(4));
-            setLat(map.getCenter().lat.toFixed(4));
-            setZoom(map.getZoom().toFixed(2));
-        });
-
-        map.addControl(new mapboxgl.NavigationControl(), "bottom-left");
-
-        return () => map.remove();
-    }, []);
+    const changeView = (type, value) => {
+        type === "lat"
+            ? setViewport({
+                  ...viewport,
+                  latitude: +value,
+                  transitionDuration: 2000,
+                  transitionInterpolator: new FlyToInterpolator(),
+              })
+            : setViewport({
+                  ...viewport,
+                  longitude: +value,
+                  transitionDuration: 2000,
+                  transitionInterpolator: new FlyToInterpolator(),
+              });
+    };
 
     return (
         <div className="wrapper">
-            <Sidebar lat={lat} lng={lng} zoom={zoom} />
-            <div className="map" ref={mapRef}></div>
+            <div className="map">
+                <Sidebar
+                    lat={viewport.latitude}
+                    lng={viewport.longitude}
+                    changeView={changeView}
+                    zoom={viewport.zoom.toFixed(2)}
+                />
+                <ReactMapGL
+                    {...viewport}
+                    mapboxApiAccessToken={accessToken}
+                    mapStyle="mapbox://styles/hannah-developer/cknrt5a4011lh17mseb8f1kt4"
+                    onViewportChange={viewport => setViewport(viewport)}
+                >
+                    <div className="controller">
+                        <NavigationControl />
+                    </div>
+                </ReactMapGL>
+            </div>
         </div>
     );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-export default function Sidebar({ lng, lat, zoom }) {
+export default function Sidebar({ lng, lat, zoom, changeView }) {
     return (
-        <div className="sidebar">
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
+        <form>
+            <div className="sidebar">
+                Latitude:{" "}
+                <input
+                    type="number"
+                    data-name="lat"
+                    defaultValue={lat}
+                    step="0.01"
+                    min="-180"
+                    max="180"
+                    onChange={e =>
+                        changeView(e.target.dataset.name, e.target.value)
+                    }
+                />{" "}
+                | Longitude:{" "}
+                <input
+                    type="number"
+                    data-name="lng"
+                    defaultValue={lng}
+                    step="0.01"
+                    min="-90"
+                    max="90"
+                    onChange={e =>
+                        changeView(e.target.dataset.name, e.target.value)
+                    }
+                />{" "}
+                | Zoom: {zoom}
+            </div>
+        </form>
     );
 }
